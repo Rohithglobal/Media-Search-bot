372c70bba8f52cb3a68bebfaaff06cac00e6705c
DheerajReji <85474968+DheerajReji@users.noreply.github.com>
2021-08-10 18:03:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -163,7 +163,7 @@ async def group(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, filter={'$and': [{'year': int(year)}]}, reply_markup=InlineKeyboardMarkup(buttons)) 
+            await message.reply_photo(photo=poster, caption=f"<b>🎬Movie/series: {search} ‌‌‌‌‎</b>\n<b>🌟🌟🌟🌟🌟|IMDb</b>\n<b>🎭Genres:</b>\n<b>© AMIGO CINEMAS</b>", reply_markup=InlineKeyboardMarkup(buttons)) 
         else:
             await message.reply_text(f"<b>📂𝙼𝙾𝚅𝙸𝙴 𝙽𝙰𝙼𝙴 :-‎ {search} ‌‌‌‌ </b>\n<code>𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝙰𝙼𝙸𝙶𝙾 𝙲𝙸𝙽𝙴𝙼𝙰𝚂</code>", reply_markup=InlineKeyboardMarkup(buttons))
 
